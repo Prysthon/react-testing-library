066f0ba71330df4c58519a9a8628ffc20d67224e
Tiago <tiagoprysthon14@gmail.com>
2022-09-24 13:21:59 -0300

    R4 - Done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('FavoritePokemons component - Req 03', () => {
+  it('shoul have a h2 with "Page requested not found"', () => {
+    renderWithRouter(<NotFound />);
+    const pageNotFoundText = screen.getByText(/Page requested not found/i);
+    expect(pageNotFoundText).toBeInTheDocument();
+  });
+
+  it('should be 1 img', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img');
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
