8a3be84773fb00ebf269af3ac8a9efb8bdceb3a5
Tiago <tiagoprysthon14@gmail.com>
2022-09-24 13:17:05 -0300

    R3 - Done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('FavoritePokemons component - Req 03', () => {
+  it('should be informations about Pokédex', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const favoritePokemons = screen.getByText(/No favorite pokemon found/i);
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
