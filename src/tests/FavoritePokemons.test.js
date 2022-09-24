616fdb7fab5502c9bcc73439f3a6feb6e2e2a5ff
Tiago <tiagoprysthon14@gmail.com>
2022-09-24 13:18:06 -0300

    R3 - change text

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,7 +4,7 @@ import { FavoritePokemons } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 
 describe('FavoritePokemons component - Req 03', () => {
-  it('should be informations about Pokédex', () => {
+  it('should be a text "No favorite pokemon found"', () => {
     renderWithRouter(<FavoritePokemons />);
     const favoritePokemons = screen.getByText(/No favorite pokemon found/i);
     expect(favoritePokemons).toBeInTheDocument();
