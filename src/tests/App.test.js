e4e813c8e7c81e3ad733e64d1915a717442a1ee0
Tiago <tiagoprysthon14@gmail.com>
2022-09-24 12:49:41 -0300

    R1 - Done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react'
+import { screen } from '@testing-library/react';
+import App from '../App.js';
+import renderWithRouter from '../renderWithRouter'
+import userEvent from '@testing-library/user-event';
+
+describe ('App component - Req 01', () => {
+  it('should be links in the nav', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favoritePokemons = screen.getByText('Favorite Pokémons');
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('should be redirect to "/" when clicked "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link',{name: /Home/i});
+    userEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  
+  it('should be redirect to "/about" when clicked "About"', () => {
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link',{name: /About/i});
+    userEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('should be redirect to "/favorites" when clicked "Pokémons Favoritados"', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoritePokemons = screen.getByRole('link',{name: /Favorite Pokémons/i});
+    userEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+})
+
